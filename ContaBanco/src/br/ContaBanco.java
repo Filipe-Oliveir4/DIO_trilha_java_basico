8d4e5a677bf353c5201bb2ea947ba5c157a2f590
Filipe Miranda de Oliveira <108041537+Filipe-Oliveir4@users.noreply.github.com>
2023-08-23 15:24:45 -0300

    Subindo o projeto conta banco

---
+++ ContaBanco/src/br/ContaBanco.java
@@ -0,0 +1,28 @@
+package br;
+
+import java.util.Scanner;
+
+public class ContaBanco {
+
+	public static void main(String[] args) {
+		//Abertura do Scanner
+		Scanner ent = new Scanner(System.in);
+		//Declaração de variáveis
+		int numero;
+		String agencia;
+		String nomeCliente;
+		double saldo;
+		//Input de dados
+		System.out.println("Por favor, digite o numero da agencia");
+		agencia=ent.nextLine();
+		System.out.println("Informe o nome do cliente");
+		nomeCliente=ent.nextLine();
+		System.out.println("Informe o numero da conta");
+		numero=ent.nextInt();
+		System.out.println("Informe o saldo");
+		saldo=ent.nextDouble();
+		System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");
+
+	}
+
+}
