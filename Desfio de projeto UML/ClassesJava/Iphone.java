c445258c38a4db0b11375c721c0734770ab9e89c
Filipe <lipimiranda1999@gmail.com>
2024-08-01 11:45:21 -0300

    Adicionando desafio de uml

---
+++ Desfio de projeto UML/ClassesJava/Iphone.java
@@ -0,0 +1,50 @@
+public class Iphone implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical{
+    private String IMEI;
+    private String contaApple;
+
+    @Override
+    public void ligar(String numero) {
+
+    }
+
+    @Override
+    public void atender() {
+
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+
+    }
+
+    @Override
+    public void novaAba(String url) {
+
+    }
+
+    @Override
+    public void atualizarPagina() {
+
+    }
+
+    @Override
+    public void tocar() {
+
+    }
+
+    @Override
+    public void pausar() {
+
+    }
+
+    @Override
+    public void selecionar() {
+
+    }
+}
+
